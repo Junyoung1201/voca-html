0977176a64c12590baf562ccb49774a331d51cab
Junyoung1025 <leejunyoung5819@gmail.com>
2025-04-16 07:41:21 +0900

    Update speak.js

--- speak.js
+++ speak.js
@@ -1,4 +1,7 @@
 function speak(text, lang) {
+
+    text = text.replaceAll("~","").replaceAll("(","").replaceAll(")",".");
+    
     return new Promise(function (resolve) {
         var utterance = new SpeechSynthesisUtterance(text);
         utterance.lang = lang || "en-US";
